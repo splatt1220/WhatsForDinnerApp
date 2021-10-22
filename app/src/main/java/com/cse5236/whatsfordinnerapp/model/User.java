b0c05530ec29ce029fa04a143be2e12f25fc6ca0
Sarah Platt <splatt1220@gmail.com>
2021-10-22 18:00:01 -0400

    registering works

--- app/src/main/java/com/cse5236/whatsfordinnerapp/model/User.java
+++ app/src/main/java/com/cse5236/whatsfordinnerapp/model/User.java
@@ -19,5 +19,19 @@ public class User {
 //        mDislikes = new ArrayList<>();
     }
 
+    public String getName() {
+        return mName;
+    }
 
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String email) {
+        mEmail = email;
+    }
 }
